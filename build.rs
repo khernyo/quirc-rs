44fa1d4058351a48a9e4576963d94deb8aa17bf9
Szabolcs Berecz <szabolcs.berecz@gmail.com>
2019-03-03 02:29:53 +0100

    Generate quirc wrapper

---
+++ build.rs
@@ -0,0 +1,36 @@
+use std::env;
+use std::fs;
+use std::io;
+use std::path::Path;
+use std::process::Command;
+
+fn main() -> Result<(), io::Error> {
+    let quirc_path = Path::new("quirc_wrapper/quirc");
+    let quirc_src_path = quirc_path.join("lib");
+    let lib_filename = "libquirc.a";
+
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let out_path = Path::new(&out_dir);
+
+    Command::new("make").current_dir(quirc_path).arg(lib_filename).status()?;
+    fs::copy(quirc_path.join(lib_filename), out_path.join(lib_filename))?;
+    Command::new("make").current_dir(quirc_path).arg("clean").status()?;
+
+    println!("cargo:rustc-link-lib=static=quirc");
+    println!("cargo:rustc-link-search=native={}", out_dir);
+
+    let bindings = bindgen::Builder::default()
+        .clang_args(&["-I", quirc_src_path.to_str().unwrap()])
+        .derive_debug(true)
+        .derive_eq(true)
+        .derive_partialeq(true)
+        .impl_debug(false)
+        .impl_partialeq(false)
+        .header("quirc_wrapper/wrapper.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    bindings.write_to_file(out_path.join("bindings.rs"))?;
+
+    Ok(())
+}
---
+++ quirc_wrapper/wrapper.h
@@ -0,0 +1,2 @@
+#include "quirc.h"
+#include "quirc_internal.h"
